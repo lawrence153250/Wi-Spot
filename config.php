2e8f40de7bf59c482b966cd0328812690001f58e
lawrence153250 <lawrencegarcia153250@gmail.com>
2025-06-22 01:20:32 +0800

    db connection update

--- config.php
+++ config.php
@@ -6,13 +6,8 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
-// Database credentials
-// define('DB_HOST', 'sql311.infinityfree.com');
-// define('DB_USER', 'if0_38382334');
-// define('DB_PASS', 'PAyQbh24YXB');
-// define('DB_NAME', 'if0_38382334_wispotdb');
 
-// $conn = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME);
+// $conn = new mysqli('sql311.infinityfree.com', 'if0_38382334', 'PAyQbh24YXB', 'if0_38382334_wispotdb');
 
 $conn = new mysqli('localhost', 'root', '', 'capstonesample');
 // Create database connection
